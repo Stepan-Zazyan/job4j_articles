1f973d86eb0f76fc9236065f5928d0f0c5565b80
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-08-05 15:39:45 +0300

    To loop savings in database Try

---
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -0,0 +1,37 @@
+package ru.job4j.articles.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.articles.model.Article;
+import ru.job4j.articles.model.Word;
+import ru.job4j.articles.service.generator.ArticleGenerator;
+import ru.job4j.articles.store.Store;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SimpleArticleService implements ArticleService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleArticleService.class.getSimpleName());
+
+    private final ArticleGenerator articleGenerator;
+
+    public SimpleArticleService(ArticleGenerator articleGenerator) {
+        this.articleGenerator = articleGenerator;
+    }
+//генерит статьи из слов в список, а потом их сохраняет
+    @Override
+    public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
+        LOGGER.info("Геренация статей в количестве {}", count);
+        List<Word> words = wordStore.findAll();
+        for (int j = 0; j < 100; j++) {
+            int finalJ = j;
+            List<Article> articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
+                    .peek(i -> LOGGER.info("Сгенерирована статья № {}", i + (finalJ * 10000)))
+                    .mapToObj((x) -> articleGenerator.generate(words))
+                    .collect(Collectors.toList());
+            articles.forEach(articleStore::save);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/articles/store/ArticleStore.java
@@ -0,0 +1,97 @@
+package ru.job4j.articles.store;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.articles.model.Article;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class ArticleStore implements Store<Article>, AutoCloseable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArticleStore.class.getSimpleName());
+
+    private final Properties properties;
+
+    private Connection connection;
+
+    public ArticleStore(Properties properties) {
+        this.properties = properties;
+        initConnection();
+        initScheme();
+    }
+
+    private void initConnection() {
+        LOGGER.info("Создание подключения к БД статей");
+        try {
+            connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password")
+            );
+        } catch (SQLException throwables) {
+            LOGGER.error("Не удалось выполнить операцию: { }", throwables.getCause());
+            throw new IllegalStateException();
+        }
+    }
+
+    private void initScheme() {
+        LOGGER.info("Инициализация таблицы статей");
+        try (Statement statement = connection.createStatement()) {
+            String sql = Files.readString(Path.of("db/scripts", "articles.sql"));
+            statement.execute(sql);
+        } catch (Exception e) {
+            LOGGER.error("Не удалось выполнить операцию: { }", e.getCause());
+            throw new IllegalStateException();
+        }
+    }
+
+    @Override
+    public Article save(Article model) {
+        LOGGER.info("Сохранение статьи");
+        String sql = "insert into articles(text) values(?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, model.getText());
+            statement.executeUpdate();
+            ResultSet key = statement.getGeneratedKeys();
+            while (key.next()) {
+                model.setId(key.getInt(1));
+            }
+        } catch (Exception e) {
+            LOGGER.error("Не удалось выполнить операцию: { }", e.getCause());
+            throw new IllegalStateException();
+        }
+        return model;
+    }
+
+    @Override
+    public List<Article> findAll() {
+        LOGGER.info("Загрузка всех статей");
+        String sql = "select * from articles";
+        ArrayList<Article> articles = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet selection = statement.executeQuery();
+            while (selection.next()) {
+                articles.add(new Article(
+                        selection.getInt("id"),
+                        selection.getString("text")
+                ));
+            }
+        } catch (Exception e) {
+            LOGGER.error("Не удалось выполнить операцию: { }", e.getCause());
+            throw new IllegalStateException();
+        }
+        return articles;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
